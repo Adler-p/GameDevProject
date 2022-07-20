e30fef6977fadbd81c4e24668639d026e32b133f
adler-p <panfeng9794@gmail.com>
2022-07-20 20:58:29 +0800

    added iced enviornment set, expaned on lvl 1, added sound assets

--- Assets/Scripts/GroundControl.cs
+++ Assets/Scripts/GroundControl.cs
@@ -7,7 +7,8 @@ public class GroundControl : MonoBehaviour
     public Sprite Sprites;
     public GameObject vfx;
     private bool isChange = false;
-    //玩家1或2碰撞改变方块地形
+
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.tag == "Player" && collision.transform.position.y > transform.position.y && !isChange)
